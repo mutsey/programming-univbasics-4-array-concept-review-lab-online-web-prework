277b29053899e1385b6ac39934dfe6955ac801a8
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-09 06:04:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,31 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+cats = ["black", "red", "orange"]
+ 
+cats.index("red")
+#=> 1
+ 
+cats.index("yellow")
+#=> nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+max_value = [1,2,1,3,4,3,5,4,3,2,1])
+ 
+max_value.index("5")
+#=> 1
+ 
+cats.index("7")
+#=> nil
+ 
 end
 
 def find_min_value(array)
-  # Add your solution here
+min_value = [10,5,3,7,19,1,3,10])
+ 
+min_value.index("1")
+#=> 1
+ 
+min_value.index("7")
+#=> nil
+ 
 end
